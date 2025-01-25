b82e4e627ea87546cdd229821c34ec491f1ae19b
Athul Babu <athul.6560@gmail.com>
2025-01-25 00:16:26 -0600

    Initial commit

---
+++ src/main/java/com/zeezaglobal/inventory_management_backend/InventoryManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zeezaglobal.inventory_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeezaglobal/inventory_management_backend/InventoryManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeezaglobal.inventory_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
